08f36960e239e810cdd916a2d2e10b88d9e59dcf
Derek Jass <jazzer36@gmail.com>
2015-05-12 15:29:48 -0500

    re-add python solutions

---
+++ ProjectEulerPython/problem003.py
@@ -0,0 +1,2 @@
+from euler.util import prime_factorization
+print(max(prime_factorization(600851475143))[0])
---
+++ ProjectEulerPython/problem010.py
@@ -0,0 +1,5 @@
+from euler.util import primes
+prime_sum = 0
+for prime in primes(2000000):
+    prime_sum += prime
+print(prime_sum)
