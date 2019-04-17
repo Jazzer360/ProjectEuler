cfd1901332fa11933a1d9928edcc1d8f6c2b3954
Derek Jass <jazzer36@gmail.com>
2019-04-17 14:44:44 -0500

    problem running setup

--- python/euler.py
+++ python/euler.py
@@ -1,13 +1,35 @@
-import problems
+from problems import problem, all_problems
+from time import time
 
 
-def run_problem(n):
-    pass
+def run_problems(problem_list):
+    for p in problem_list:
+        n = int(p.__name__[-3:])
+        print('Solving problem {:d}'.format(n))
+        start_t = time()
+        ans = p.solve()
+        end_t = time()
+        print('  Answer: {}'.format(ans))
+        if getattr(p, 'ANSWER', None):
+            if p.ANSWER != ans:
+                print('    Incorrect answer.')
+        print('  Time: {:.0f} ms\n'.format((end_t - start_t) * 1000))
+
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a')
-    parser.parse_args()
+    parser.add_argument('problems', help='problems to run',
+                        nargs='*', type=int)
+    args = parser.parse_args()
 
-    print(dir(problems.problem001))
+    if args.problems:
+        p_list = []
+        for n in args.problems:
+            try:
+                p_list.append(problem(n))
+            except ModuleNotFoundError:
+                print('Problem {:d} not found'.format(n))
+    else:
+        p_list = all_problems()
+    run_problems(p_list)
--- python/problems/__init__.py
+++ python/problems/__init__.py
@@ -1,5 +1,16 @@
-from os.path import dirname, basename, isfile
-import glob
-modules = glob.glob(dirname(__file__)+"/*.py")
-__all__ = [basename(f)[:-3] for f in modules 
-           if isfile(f) and not f.endswith('__init__.py')]
+from importlib import import_module
+from pathlib import Path
+import re
+
+
+def problem(n):
+    n = int(n)
+    return import_module('.problem{:03d}'.format(n), __name__)
+
+
+def all_problems():
+    path = Path(__file__).resolve().parent
+    regex = re.compile(r'problem(\d{3})\.py')
+    matches = (regex.match(f.name) for f in path.iterdir())
+    probs = (m[1] for m in matches if m)
+    return (problem(n) for n in probs)
